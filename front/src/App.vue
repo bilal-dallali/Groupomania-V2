db709bcffff209cf80ff4519fea8a0df36f7784e
bilal-webdeveloper <dallali-bilal@hotmail.fr>
2023-01-07 22:33:30 +0100

    adding vue bootstrap and first component

---
+++ front/src/App.vue
@@ -0,0 +1,20 @@
+<script>
+import LoginVue from './components/Login.vue';
+
+export default {
+  name: "App",
+  components: {
+    LoginVue
+  }
+}
+</script>
+
+<template>
+  <h1>Groupomania</h1>
+  <LoginVue />
+</template>
+
+<style scoped>
+
+
+</style>
